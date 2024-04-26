a444107e6e5df691bc7e098b1e925e52f6ffa048
sabermhareshdj <dj.saber.mharesh.com>
2024-04-26 20:38:38 +0300

    add css inheritance

--- product/views.py
+++ product/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
-from django.views.generic import ListView , DeleteView
+from django.views.generic import ListView , DetailView
 from .models import Product , ProductImages , Brand , Review
 
 class ProductList(ListView):
   model = Product             # context : object_list , model_list 
 
-class ProductDetail(DeleteView):
+class ProductDetail(DetailView):
   model = Product             # context : object , model
