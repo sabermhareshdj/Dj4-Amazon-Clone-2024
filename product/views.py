bcc6d61b19f37008a19f5e4fb51ce8af80fe4b03
sabermhareshdj <dj.saber.mharesh.com>
2024-04-26 22:12:35 +0300

    add product reviews

--- product/views.py
+++ product/views.py
@@ -7,3 +7,8 @@ class ProductList(ListView):
 
 class ProductDetail(DetailView):
   model = Product             # context : object , model
+
+  def get_context_data(self, **kwargs):
+    context = super().get_context_data(**kwargs)
+    context["reviews"] = Review.objects.filter(product=self.get_object())
+    return context
