cabcdfd4db23b9615812bd18744036f9b91f3c4d
sabermhareshdj <dj.saber.mharesh.com>
2024-04-29 19:05:00 +0300

    add customs pagination

--- product/api.py
+++ product/api.py
@@ -3,6 +3,7 @@ from .serializers import ProductListSerilizer, ProductDetailSerilizer ,BrandDeta
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
 from .myfilter import ProductFilter
+from .mypagination import MyPagination
 from .models import Product , Brand
 from rest_framework import generics
 
@@ -31,6 +32,7 @@ class ProductListAPI(generics.ListCreateAPIView):
   search_fields = ['name', 'subtitle','description']
   ordering_fields = ['price', 'quantity']
   filterset_class = ProductFilter
+  pagination_class = MyPagination
 
 
 class ProductDetailAPI(generics.RetrieveUpdateDestroyAPIView):
