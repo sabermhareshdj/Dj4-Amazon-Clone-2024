4665e43ade36eec68bbcaa770a814515bdf93935
sabermhareshdj <dj.saber.mharesh.com>
2024-04-29 14:57:29 +0300

    add brand in product list , add reviews_count , avg_rate product detail

--- product/serializers.py
+++ product/serializers.py
@@ -3,18 +3,28 @@ from django.db.models.aggregates import Avg
 from .models import Product , Brand
 
 
+class BrandListSerilizer(serializers.ModelSerializer):
+  class Meta:
+    model = Brand
+    fields = '__all__'
+
+
 
 class ProductListSerilizer(serializers.ModelSerializer):
+  # brand = BrandListSerilizer() # تفصيل brand كامل 
+  brand = serializers.StringRelatedField()
   avg_rate = serializers.SerializerMethodField()
   reviews_count = serializers.SerializerMethodField()
-  price_with_tax = serializers.SerializerMethodField()
+  # price_with_tax = serializers.SerializerMethodField()
 
   class Meta:
     model = Product
     fields = '__all__'
 
-  def get_price_with_tax(self,product):
-    return product.price*1.5
+
+  # ضريبة على المنتج 
+  # def get_price_with_tax(self,product):
+  #   return product.price*1.5
 
   def get_avg_rate(self,product):
     avg = product.review_product.aggregate(rate_avg=Avg('rate'))
@@ -32,17 +42,29 @@ class ProductListSerilizer(serializers.ModelSerializer):
 
 
 class ProductDetailSerilizer(serializers.ModelSerializer):
+  avg_rate = serializers.SerializerMethodField()
+  reviews_count = serializers.SerializerMethodField()
+
   class Meta:
     model = Product
     fields = '__all__'
 
+  def get_avg_rate(self,product):
+    avg = product.review_product.aggregate(rate_avg=Avg('rate'))
+    if not avg['rate_avg'] :
+      result = 0
+      return result
+    return avg['rate_avg']
   
 
+  def get_reviews_count(self,product:Product):
+    reviews = product.review_product.all().count()
+    return reviews
 
-class BrandListSerilizer(serializers.ModelSerializer):
-  class Meta:
-    model = Brand
-    fields = '__all__'
+    
+
+
+
 
 
 class BrandDetailSerilizer(serializers.ModelSerializer):
