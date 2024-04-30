8cb5e80eaada2d8cda1293417841a586a8367465
sabermhareshdj <dj.saber.mharesh.com>
2024-04-30 17:23:13 +0300

    design Order , OrderDetail , Cart , CartDetail , Coupon

--- analysis.md
+++ analysis.md
@@ -21,3 +21,20 @@ Product :
 - description
 - related items 
 - quantity
+________________________________________
+
+orders :
+  - code 
+  - order time
+  - delivery time
+  - address
+  - discount 
+  - total products
+  - delivery fee 
+  - status [recieved, Processed, Shipped, Delivered]
+  - products
+    - name
+    - image
+    - price
+    - quantity
+    - brand 
--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,80 @@
+from typing import Iterable
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
+import datetime
+from utils.generate_code import generate_code
+from product.models import Product
 
-# Create your models here.
+
+CART_STATUS =(
+  ('InProgress','InProgress'),
+  ('Completed','Completed')
+)
+
+class Cart(models.Model):
+  user = models.ForeignKey(User,related_name='cart_user',on_delete=models.SET_NULL,null=True,blank=True)
+  status = models.CharField(max_length=10,choices=CART_STATUS)
+
+  def __str__(self):
+    return str(self.user)
+
+
+class CartDetail(models.Model):
+  cart = models.ForeignKey(Cart,related_name='cart_detail',on_delete=models.CASCADE)
+  product = models.ForeignKey(Product,related_name='cart_product',on_delete=models.SET_NULL,null=True,blank=True)
+  quantity = models.IntegerField()
+  total = models.FloatField(null=True,blank=True)
+
+  def __str__(self):
+    return str(self.cart)
+
+
+ORDER_STATUS =(
+  ('Recieved','Recieved'),
+  ('Processed','Processed'),
+  ('Shipped','Shipped'),
+  ('Delivered','Delivered'),
+)
+
+class Order(models.Model):
+  user = models.ForeignKey(User,related_name='order_user',on_delete=models.SET_NULL,null=True,blank=True)
+  status = models.CharField(max_length=10,choices=ORDER_STATUS)
+  code = models.CharField(max_length=8,default=generate_code())
+  order_time = models.DateField(default=timezone.now)
+  delivery_time = models.DateField(null=True,blank=True)
+  coupon = models.ForeignKey('Coupon',related_name='order_coupon',on_delete=models.SET_NULL ,null=True,blank=True)
+  total_after_coupon = models.FloatField(null=True,blank=True)
+
+  def __str__(self):
+    return str(self.user)
+
+
+
+class OrderDetail(models.Model):
+  order = models.ForeignKey(Order,related_name='order_detail',on_delete=models.CASCADE)
+  product = models.ForeignKey(Product,related_name='order_product',on_delete=models.SET_NULL,null=True,blank=True)
+  price = models.FloatField()
+  quantity = models.IntegerField()
+  total = models.FloatField(null=True,blank=True)
+
+  def __str__(self):
+    return str(self.order)
+
+
+
+class Coupon(models.Model):
+  code = models.CharField(max_length=20)
+  discount = models.IntegerField()
+  quantity = models.IntegerField()
+  start_date = models.DateField(default=timezone.now)
+  end_date = models.DateField(null=True,blank=True)
+
+  def __str__(self):
+    return self.code
+  
+
+  def save(self, *args, **kwargs):
+    week = datetime.timedelta(days=7)
+    self.end_date = self.start_date + week 
+    super(coupon,self).save(*args, **kwargs)
