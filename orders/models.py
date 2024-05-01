e45259944af5ed9db7ca89dbce0d43c33368104a
sabermhareshdj <dj.saber.mharesh.com>
2024-05-01 12:09:53 +0300

    show cart total in base , add in models

--- orders/models.py
+++ orders/models.py
@@ -18,6 +18,13 @@ class Cart(models.Model):
 
   def __str__(self):
     return str(self.user)
+  
+  def cart_total(self):
+    total = 0
+    for item in self.cart_detail.all():
+      total += item.total
+
+    return total
 
 
 class CartDetail(models.Model):
