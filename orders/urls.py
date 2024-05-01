8cb6f33d81bf9a1c27b2953981f9d8d9c6dc4964
sabermhareshdj <dj.saber.mharesh.com>
2024-05-01 18:22:51 +0300

    add checkout page p1

--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
-from .views import OrderList
+from .views import OrderList , checkout
 
 urlpatterns = [
     path('', OrderList.as_view()),
-  
+    path('checkout', checkout),
 ]
