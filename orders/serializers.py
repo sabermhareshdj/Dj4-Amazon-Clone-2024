2f857b8ef566074ccb0fe773314b6f4a382d1b57
sabermhareshdj <dj.saber.mharesh@gmail.com>
2024-05-02 17:10:19 +0300

    add order serializers

--- orders/serializers.py
+++ orders/serializers.py
@@ -3,8 +3,8 @@ from .models import Cart , CartDetail , Order , OrderDetail
 from product.serializers import ProductListSerilizer
 
 class CartDetailSerializer(serializers.ModelSerializer):
-  # product = ProductListSerilizer()
-  product = serializers.StringRelatedField()
+  product = ProductListSerilizer()
+  #product = serializers.StringRelatedField()
   class Meta:
     model = CartDetail
     fields = '__all__'
@@ -16,3 +16,22 @@ class CartSerializer(serializers.ModelSerializer):
     fields = '__all__'
 
 
+class OrderListSerializer(serializers.ModelSerializer):
+  user = serializers.StringRelatedField()
+  class Meta:
+    model = Order
+    fields = '__all__'
+
+
+class OrderProductSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = OrderDetail
+    fields = '__all__'
+
+
+class OrderDetailSerializer(serializers.ModelSerializer):
+  user = serializers.StringRelatedField()
+  products = OrderProductSerializer(many=True,source='order_detail')
+  class Meta:
+    model = Order
+    fields = '__all__'
